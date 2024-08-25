7882e2257e9df1f6345f78d630a78129d5e75f0b
Pekka P <pekka.prepula@gmail.com>
2024-08-25 17:25:21 +0300

    enhance homepage resonsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,15 +5,16 @@ import Quizzer from "../components/quizzer"
 const Home: React.FC = () => {
   return (
     <Container
-      maxW="container.md"
-      py={{ base: 6, md: 10 }}
-      px={{ base: 4, md: 6 }}
+      maxW={{ base: "100%", md: "container.md" }}
+      py={{ base: 4, md: 10 }}
+      px={{ base: 2, md: 6 }}
     >
       <Box
         textAlign="center"
         borderRadius="md"
         boxShadow="sm"
-        p={{ base: 4, md: 8 }}
+        p={{ base: 3, md: 8 }}
+        fontSize={{ base: "md", md: "xl" }}
         bg="#E2DAD6"
         _hover={{ boxShadow: "md" }}
         transition="box-shadow 0.2s"
